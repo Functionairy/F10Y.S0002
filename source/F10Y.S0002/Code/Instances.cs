554a6b7a5be21bb75421961ebaeb9bad35563604
davidcoats <david@getsafetycone.com>
2025-11-25 10:38:41 -0800

    Interim commit.

--- source/F10Y.S0002/Code/Instances.cs
+++ source/F10Y.S0002/Code/Instances.cs
@@ -9,7 +9,7 @@ namespace F10Y.S0002
         public static L0000.IConsoleOperator ConsoleOperator => L0000.ConsoleOperator.Instance;
         public static L0001.L000.IDriveInfoOperator DriveInfoOperator => L0001.L000.DriveInfoOperator.Instance;
         public static L0000.IEnumerableOperator EnumerableOperator => L0000.EnumerableOperator.Instance;
-        public static L0060.IEnvironmentOperator EnvironmentOperator => L0060.EnvironmentOperator.Instance;
+        public static L0061.IEnvironmentOperator EnvironmentOperator => L0061.EnvironmentOperator.Instance;
         public static L0000.IMachineNameOperator MachineNameOperator => L0000.MachineNameOperator.Instance;
         public static L0001.L000.IUnitSymbols UnitSymbols => L0001.L000.UnitSymbols.Instance;
     }
